24686d0c1bcfa19deb1e27327cccf1f9d27e1609
Chocoford <504001583@qq.com>
2023-10-09 13:10:36 +0800

    Update Color+Extension.swift

--- Sources/TrickleUI/Extensions/Color+Extension.swift
+++ Sources/TrickleUI/Extensions/Color+Extension.swift
@@ -35,21 +35,21 @@ extension Color {
             case .dark:
                 switch selectOptionColor {
                     case .red:
-                        self.init(hexString: "#DA5959")
+                        self.init(hexString: "#e80707")
                     case .green:
-                        self.init(hexString: "#3AAD64")
+                        self.init(hexString: "#12d956")
                     case .yellow:
-                        self.init(hexString: "#E2BE2D")
+                        self.init(hexString: "#e8d40a")
                     case .blue:
-                        self.init(hexString: "#70A7EA")
+                        self.init(hexString: "#0666e6")
                     case .orange:
-                        self.init(hexString: "#E2BE2D")
+                        self.init(hexString: "#ea8500")
                     case .pink:
-                        self.init(hexString: "#D75D99")
+                        self.init(hexString: "#d61a86")
                     case .purple:
-                        self.init(hexString: "#A769E3")
+                        self.init(hexString: "#7400f3")
                     case .gray:
-                        self.init(hexString: "#FFFFFF17")
+                        self.init(hexString: "#646e83")
                 }
                 
             @unknown default:
@@ -57,7 +57,65 @@ extension Color {
         }
     }
     
-    public init (trickleColorName name: String, scheme: ColorScheme = .light) {
-        self = Color(FieldOptions.FieldOptionInfo.FieldOptionColor.init(rawValue: name) ?? .gray, scheme: scheme)
+    public enum TrickleColor: String {
+        case red
+        case green
+        case yellow
+        case blue
+        case orange
+        case pink
+        case purple
+        case gray
+    }
+    
+    public init(trickleColor color: TrickleColor, scheme: ColorScheme = .light) {
+        switch scheme {
+            case .light:
+                switch color {
+                    case .red:
+                        self.init(hexString: "#EF4444")
+                    case .green:
+                        self.init(hexString: "#22C55E")
+                    case .yellow:
+                        self.init(hexString: "#FACC15")
+                    case .blue:
+                        self.init(hexString: "#60A5FA")
+                    case .orange:
+                        self.init(hexString: "#FB923C")
+                    case .pink:
+                        self.init(hexString: "#EC4899")
+                    case .purple:
+                        self.init(hexString: "#A855F7")
+                    case .gray:
+                        self.init(hexString: "#E5E7EB")
+                }
+                
+            case .dark:
+                switch color {
+                    case .red:
+                        self.init(hexString: "#FEE2E2")
+                    case .green:
+                        self.init(hexString: "#DCFCE7")
+                    case .yellow:
+                        self.init(hexString: "#FEFCE8")
+                    case .blue:
+                        self.init(hexString: "#DBEAFE")
+                    case .orange:
+                        self.init(hexString: "#FFEDD5")
+                    case .pink:
+                        self.init(hexString: "#FCE7F3")
+                    case .purple:
+                        self.init(hexString: "#F3E8FF")
+                    case .gray:
+                        self.init(hexString: "#E5E7EB")
+                }
+                
+            @unknown default:
+                fatalError()
+        }
+    }
+    
+    public init(trickleColorName name: String, scheme: ColorScheme = .light) {
+        self.init(trickleColor: TrickleColor(rawValue: name) ?? .gray, scheme: scheme)
     }
 }
